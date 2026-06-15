6093982ada2b0fd23c839f93c5cd0e1795646e72
Ahmed Iftikhar <ahmed.iftikhar.fsd@gmail.com>
2026-06-15 15:03:28 +0500

    fix register page

--- lib/features/auth/widgets/register_form.dart
+++ lib/features/auth/widgets/register_form.dart
@@ -17,6 +17,7 @@ class RegisterForm extends ConsumerStatefulWidget {
 
 class _RegisterFormState extends ConsumerState<RegisterForm> {
   final _formKey = GlobalKey<FormState>();
+  late bool isLoading = false;
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
@@ -62,9 +63,16 @@ class _RegisterFormState extends ConsumerState<RegisterForm> {
       final password = _passwordController.text.trim();
       // Call register API here using email and password
       final authActions = ref.read(authProvider.notifier);
+      setState(() {
+        isLoading = true;
+      });
       await authActions.register(name: name, email: email, password: password);
+      setState(() {
+        isLoading = false;
+      });
       final auth = ref.read(authProvider);
       final code = auth.asData?.value.authFailure?.code;
+      print("auth code $code");
       if (code == FirebaseAuthCodes.emailAlreadyInUse) {
         final errorMessage =
             auth.asData?.value.authFailure?.message ??
@@ -94,92 +102,106 @@ class _RegisterFormState extends ConsumerState<RegisterForm> {
     return Form(
       key: _formKey,
 
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset("assets/images/logo_icon.png", height: 100),
-          const SizedBox(height: 16),
+      child: AbsorbPointer(
+        absorbing: isLoading,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset("assets/images/logo_icon.png", height: 100),
+            const SizedBox(height: 16),
 
-          Text(
-            "Create an Account",
-            style: theme.textTheme.headlineSmall?.copyWith(
-              fontWeight: FontWeight.bold,
+            Text(
+              "Create an Account",
+              style: theme.textTheme.headlineSmall?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
 
-          const SizedBox(height: 6),
+            const SizedBox(height: 6),
 
-          Center(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
 
-              children: [
-                Text(
-                  "Already have an account?",
-                  style: theme.textTheme.bodyMedium?.copyWith(
-                    color: Colors.grey,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: theme.textTheme.bodyMedium?.copyWith(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    context.pushNamed(RouteNames.login);
-                  },
-                  child: const Text("Sign In"),
-                ),
-              ],
-            ),
-          ),
-
-          const SizedBox(height: 30),
-
-          TextFormField(
-            controller: _nameController,
-            validator: _validateName,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            decoration: InputDecoration(
-              labelText: "Name",
-              prefixIcon: Icon(
-                Icons.person_outline,
-                color: AppConstants.primaryColor,
+                  TextButton(
+                    onPressed: () {
+                      context.pushNamed(RouteNames.login);
+                    },
+                    child: const Text("Sign In"),
+                  ),
+                ],
               ),
             ),
-          ),
 
-          const SizedBox(height: 16),
+            const SizedBox(height: 30),
 
-          TextFormField(
-            controller: _emailController,
-            validator: _validateEmail,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: "Email",
-              prefixIcon: Icon(
-                Icons.email_outlined,
-                color: AppConstants.primaryColor,
+            TextFormField(
+              controller: _nameController,
+              validator: _validateName,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: InputDecoration(
+                labelText: "Name",
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  color: AppConstants.primaryColor,
+                ),
               ),
             ),
-          ),
 
-          const SizedBox(height: 16),
+            const SizedBox(height: 16),
 
-          PasswordField(
-            controller: _passwordController,
-            validator: _validatePassword,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-          ),
-
-          const SizedBox(height: 20),
-
-          SizedBox(
-            width: double.infinity,
-            height: 50,
-            child: ElevatedButton(
-              onPressed: _submit,
-              child: const Text("Register", style: TextStyle(fontSize: 16)),
+            TextFormField(
+              controller: _emailController,
+              validator: _validateEmail,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "Email",
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: AppConstants.primaryColor,
+                ),
+              ),
             ),
-          ),
-        ],
+
+            const SizedBox(height: 16),
+
+            PasswordField(
+              controller: _passwordController,
+              validator: _validatePassword,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+
+            const SizedBox(height: 20),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: isLoading ? null : _submit,
+                child: isLoading
+                    ? const SizedBox(
+                        width: 24,
+                        height: 24,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                      )
+                    : const Text("Register", style: TextStyle(fontSize: 16)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
